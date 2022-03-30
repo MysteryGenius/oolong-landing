1cb5379728f63e7cce429771623ec441144bab92
JayZ <jianzhen@doyobi.com>
2022-03-31 00:49:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-gray-800 h-screen">
+    <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+      <div class="max-w-4xl">
+        <h2 class="text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">🍵 Hello Oolong!</h2>
+        <div class="mt-10">
+          <p class="mt-5 text-xl text-gray-400">Watch this space! Great things are coming! Find me at <a href="mailto:tanjianzhen3000@gmail.com" class="underline">tanjianzhen3000@gmail.com</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
