6c8e8a5772fa04aefa086dfff2bd240296d147ac
JayZ <tanjianzhen3000@gmail.com>
2022-10-22 22:19:48 +0800

    feat: updated for project oolong

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,88 @@
       <div class="max-w-4xl">
         <h2 class="text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">🍵 Hello Oolong!</h2>
         <div class="mt-10">
-          <p class="mt-5 text-xl text-gray-400">Watch this space! Great things are coming! Find me at <a href="mailto:tanjianzhen3000@gmail.com" class="underline">tanjianzhen3000@gmail.com</a>
+          <p class="mt-5 text-xl text-gray-400">
+            Hey we are building tools to help youths build their support network.
           </p>
+          <p class="mt-5 text-xl text-gray-400">
+            We are currently in the process of building our first product, a simple tool to help users coordinate their
+            social events by
+            <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-orange-500">
+              automating the boring parts
+            </span>
+            of scheduling, location planning and more. Join our mailing list to stay up to date with our progress.
+          </p>
+          <p class="mt-5 text-xl text-gray-400">Join our mailing list to stay up to date with our progress.</p>
+
+          <!-- Begin Mailchimp Signup Form -->
+          <div id="mc_embed_signup" class="px-2 py-0.5 my-4 rounded-lg">
+            <form
+              action="https://gmail.us10.list-manage.com/subscribe/post?u=fed52bd159f3cef7ff5d8a5d8&amp;id=0e59c3fac5&amp;f_id=00823be2f0"
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              class="validate"
+              target="_blank"
+              novalidate
+            >
+              <div id="mc_embed_signup_scroll">
+                <h2>Join us for updates!</h2>
+                <div class="mc-field-group">
+                  <label for="mce-EMAIL">Email Address</label>
+                  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required />
+                  <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+                </div>
+                <div id="mce-responses" class="clear foot">
+                  <div class="response" id="mce-error-response" style="display: none"></div>
+                  <div class="response" id="mce-success-response" style="display: none"></div>
+                </div>
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px" aria-hidden="true">
+                  <input type="text" name="b_fed52bd159f3cef7ff5d8a5d8_0e59c3fac5" tabindex="-1" value="" />
+                </div>
+                <div class="optionalParent">
+                  <div class="clear foot">
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" />
+                    <p class="brandingLogo">
+                      <a href="http://eepurl.com/ibZyIz" title="Mailchimp - email marketing made easy and fun"
+                        ><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"
+                      /></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <!--End mc_embed_signup-->
         </div>
       </div>
     </div>
   </div>
 </template>
+<script type="text/javascript">
+(function ($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0] = "EMAIL";
+  ftypes[0] = "email";
+  fnames[1] = "FNAME";
+  ftypes[1] = "text";
+  fnames[2] = "LNAME";
+  ftypes[2] = "text";
+  fnames[3] = "ADDRESS";
+  ftypes[3] = "address";
+  fnames[4] = "PHONE";
+  ftypes[4] = "phone";
+  fnames[5] = "BIRTHDAY";
+  ftypes[5] = "birthday";
+})(jQuery);
+var $mcj = jQuery.noConflict(true);
+</script>
+<style type="text/css">
+#mc_embed_signup {
+  background: #fff;
+  clear: left;
+  font: 14px Helvetica, Arial, sans-serif;
+  width: 600px;
+}
+</style>
